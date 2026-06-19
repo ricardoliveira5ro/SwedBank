bbf51e62ace6b29a059ac0f6eec6bf6cebe1ad15
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-06-19 18:07:02 +0100

    Create JPA Repositories

---
+++ backend/src/main/java/com/swedbank/backend/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.swedbank.backend.repository;
+
+import com.swedbank.backend.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+}
---
+++ backend/src/main/java/com/swedbank/backend/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.swedbank.backend.repository;
+
+import com.swedbank.backend.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+}
