8c327f02c2b0a4100dd1b38c6f555c8481f01159
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2026-06-22 19:44:54 +0100

    infinite scroll

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Account } from '../models/account.model';
 import { HttpClient } from '@angular/common/http';
@@ -11,12 +11,19 @@ import { Transaction } from '../models/transaction.model';
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
-export class AccountComponent {
+export class AccountComponent implements AfterViewInit, OnDestroy {
 
   httpClient = inject(HttpClient);
   public account?: Account;
   public transactions: Array<Transaction> = [];
-  public page: number = 0;
+  public isLoading: boolean = false;
+
+  @ViewChild('sentinel') sentinel!: ElementRef<HTMLDivElement>;
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLDivElement>;
+
+  private observer: IntersectionObserver | null = null;
+  private page: number = 0;
+  private hasMorePages: boolean = true;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -36,20 +43,45 @@ export class AccountComponent {
         next: (data: any) => {
           console.log(data);
           this.transactions = data.content;
+          this.page = data.page;
+          this.hasMorePages = data.page + 1 < data.totalPages;
         }, error: (err) => console.log(err)
       });
   }
 
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && this.hasMorePages && !this.isLoading) {
+        this.loadMoreTransactions();
+      }
+    }, { root: this.scrollContainer.nativeElement });
+    this.observer.observe(this.sentinel.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.observer?.disconnect();
+  }
+
   loadMoreTransactions() {
+    if (this.isLoading || !this.hasMorePages) return;
+
     const accountId = this.route.snapshot.paramMap.get('accountId');
+    this.isLoading = true;
     this.page++;
 
-    this.httpClient.get('http://localhost:8080/api/transactions/' + accountId + '/history?page=' + this.page)
+    this.httpClient.get('http://localhost:8080/api/transactions/' + accountId + '/history?page=' + this.page + '&size=' + 5)
       .subscribe({
         next: (data: any) => {
           console.log(data);
-          this.transactions.push.apply(this.transactions, data.content);
-        }, error: (err) => console.log(err)
+          this.transactions.push(...data.content);
+          this.page = data.page;
+          this.hasMorePages = data.page + 1 < data.totalPages;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.isLoading = false;
+        }
       });
   }
 }
